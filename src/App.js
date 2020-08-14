a83426771f7176d077357fbcca75658b3e2f6e52
Daniela Sannino <daniela.sannino1@gmail.com>
2020-08-14 11:13:22 -0400

    added state and props for ToDo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import ToDo from './components/ToDo.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [
+        { description: 'Walk the cat', isCompleted: true },
+        { description: 'Throw the dishes away', isCompleted: false },
+        { description: 'Buy new dishes', isCompleted: false }
+      ]
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        <ul>
+          {this.state.todos.map((todo, index) =>
+            <ToDo key={index} description={todo.description} isCompleted={todo.isCompleted} />
+          )}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
